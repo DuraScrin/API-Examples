be7090169be5191b86a9a0b4d3904df322ca3440
DuraScrin <cccp_dura@hotmail.com>
2020-04-28 17:30:04 +0200

    authentication

---
+++ API_Example/Authorization/Authorize.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API_Example.Authorization
+{
+    public class AuthorizeService
+    {
+        public bool CheckToken(string token)
+        {
+            return token == "testtoken";
+        }
+    }
+
+    public class Authorize : Attribute, IActionFilter
+    {
+        AuthorizeService authorizer = new AuthorizeService();
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.Headers["Auth"] == "")
+            {
+                context.Result = new UnauthorizedResult();
+            }
+
+            if (authorizer.CheckToken(context.HttpContext.Request.Headers["Auth"]))
+            {
+                //do some extra stuff ex: login time
+            }
+            else
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var output = context.Result;
+        }
+    }
+}
---
+++ API_Example/Controllers/TestAuthorizeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Example.Controllers
+{
+    public class TestAuthorizeController : Controller
+    {
+        [Authorize]
+        [Route("testauthorize")]
+        public string TestAccess()
+        {
+            return "Success";
+        }
+    }
+}
